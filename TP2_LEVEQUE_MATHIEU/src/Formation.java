723c96c4bc438f523eb3cae1c59d6d800833d1b5
gabinM <gabin.mathieu4@etu.univ-lorraine.fr>
2024-09-18 11:56:53 +0200

    ajout classe Formation

---
+++ TP2_LEVEQUE_MATHIEU/src/Formation.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Formation {
+    private String identifiant;
+    private Map<String, Double> CollMatieres;
+
+    public Formation(String identifiant) {
+        this.identifiant = identifiant;
+        CollMatieres = new HashMap<String, Double>();
+    }
+
+    public void addMatiere(String matiere, Double valeur) {
+        CollMatieres.put(matiere, valeur);
+    }
+
+    public void supprimerMatiere(String matiere) {
+        CollMatieres.remove(matiere);
+    }
+
+    public double getCoeff(String matiere) {
+        if (CollMatieres.containsKey(matiere)) {
+            return CollMatieres.get(matiere);
+        }
+        return 0.0;
+    }
+
+    public String toString() {
+        String res = "";
+        for (Map.Entry<String, Double> entry : CollMatieres.entrySet()) {
+            res += entry.getKey() + ": " + entry.getValue() + "\n";
+        }
+        return res;
+    }
+}
